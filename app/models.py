532c0a8c46fa36bd12df69d16cecfd9a569ab58f
Gashara <gashara2002@gmail.com>
2024-04-07 13:14:06 -0500

    excercise 2 completed and database made

--- app/models.py
+++ app/models.py
@@ -1,1 +1,21 @@
 # Add any model classes for Flask-SQLAlchemy here
+from flask import Flask
+from . import db
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
+        self.created_at = datetime.now()
